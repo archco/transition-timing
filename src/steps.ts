83b5dbb6c42548d48c638368a0ff9a4b9c600fd7
archco <archcoster@gmail.com>
2019-12-28 12:57:00 +0900

    Update steps.ts

--- src/steps.ts
+++ src/steps.ts
@@ -1,12 +1,11 @@
 export type TimingFunction = (t: number) => number;
 
 export function steps(num: number, direction: 'start'|'end'): TimingFunction {
-  const isStart = direction === 'start' ? true : false;
+  const isStart = direction === 'start';
   return (t: number) => {
-    switch (t) {
-      case 0: return 0;
-      case 1: return 1;
-    }
+    if (t <= 0) return 0;
+    if (t >= 1) return 1;
+
     for (let i = 1; i <= num; i++) {
       const p = i / num;
       if (p > t) {
